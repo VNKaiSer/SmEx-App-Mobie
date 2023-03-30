5f3c0460908196645446ca47a7f0c3d80aebbb09
kaiser <tandatvok16@gmail.com>
2023-03-30 20:03:36 +0700

    Add screen home

--- app/src/main/java/com/example/smex_app_android/HomeLayout.java
+++ app/src/main/java/com/example/smex_app_android/HomeLayout.java
@@ -4,11 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 public class HomeLayout extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        // Hide the status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Hide the navigation bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.home);
     }
 }
